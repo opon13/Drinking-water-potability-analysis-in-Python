2e4e514fc1a22adea6fc32ae31adc2a62bf0e904
datascientist-hist <pepeee89@hotmail.it>
2022-12-27 14:31:31 +0100

    added splitting function inside prep library

--- prep.py
+++ prep.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 import random
+
+
 
 def prep(df, 
          axis: str,
@@ -49,3 +52,25 @@ def prep(df,
         return scaled_df
     else:
         return clean_df
+
+
+def splitting_func(df,perctrain=0.60,perctest=0.50):
+    X_water = df[:,0:8]
+    y_water = df[:,9]
+
+    print('BEFORE SPLITTING: \n')
+    print('X_water0 shape: ', np.shape(X_water))
+    print('y_water0 shape: ', np.shape(y_water))
+
+    X_train, X_test, y_train, y_test = train_test_split(X_water, y_water, test_size=1-perctrain, random_state=13) # 60% of total values for train
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=13) # 20% of total values for both validation and test
+
+    print('\nAFTER SPLITTING: ')
+    print('X_train0 shape: ', np.shape(X_train))
+    print('X_val0 shape: ', np.shape(X_val))
+    print('X_test0 shape: ', np.shape(X_test))
+    print('y_train0 shape: ', np.shape(y_train))
+    print('y_val0 shape: ', np.shape(y_val))
+    print('y_test0 shape: ', np.shape(y_test))
+    
+    return(X_train,X_val, X_test,y_train, y_val, y_test)
